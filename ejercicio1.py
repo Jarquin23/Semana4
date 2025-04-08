f23766f5b74e8880f07e26d6ce84f22c57c8950b
Jarquin23 <amjarquinc@uamv.edu.ni>
2025-04-08 08:50:08 -0600

    semana 4 sesion1

---
+++ ejercicio1.py
@@ -0,0 +1,13 @@
+#declarar variables
+nombre_completo = "Alfredo Miguel Jarquín Castillo"
+edad = 17
+altura = 1.76
+soltero = True
+print(f"Nombre competo: {nombre_completo}")
+print(f"Tipo de dato {type(nombre_completo)}")
+print(f"Edad. {edad}")
+print(f"Tipo de dato {type(edad)}")
+print(f"Altura: {altura}")
+print(f"Tipo de dato {type(altura)}")
+print(f"Soltero: {soltero}")
+print(f"Tipo de dato {type(soltero)}")
---
+++ ejercicio2.py
@@ -0,0 +1,10 @@
+
+
+cantidad = 10
+precio = 130
+total = cantidad * precio
+def ftexto (texto):
+    return f"{texto:<10}"
+print(f"Cantidad: {cantidad:>6}")
+print(f"Precio: {precio:>6}")
+print(f"Total {total:>6}")
